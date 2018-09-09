bb0f6f8426a504a9d2b784b69aef0039ea5ffced
konatsu <runnrairu@gmail.com>
2018-09-09 18:31:01 +0900

    Add files via upload

--- mine.py
+++ mine.py
@@ -1,37 +1,79 @@
 # -*- coding: utf-8 -*-
+"""
+Created on Tue Sep  4 03:54:38 2018
 
+@author: 宮本来夏
 """
 
 import numpy as np
+import tensorflow as tf
+
+
+def mnist_double(batch_y,task):#mnistを二値分類に帰着
+    n = len(batch_y)
+    
+    y_new = [[0 for j in range(1)] for i in range(n)]
+    if task == "OrS":
+        for i in range(n):
+            if batch_y[i][0] == 1:
+                y_new[i][0]= 100000.0
+            else:
+                y_new[i][0]=-100000.0
+                
+                
+    elif task =="label":
+        
+        for i in range(n):
+            if batch_y[i][0] == 1:
+                y_new[i][0] = 1.0
+            else:
+                y_new[i][0]= 0.0
+        
+        
+    else:
+        print ("error")
+    return y_new
+
+
+
+def double_class(y,n):
+    
+    y_new = [0]*(n)
+    for i in range(n):
+        y_new[i] = tf.cond(y[i][0]>0.5,lambda:1.0,lambda:0.0)
+            
+    return y_new
+            
+    
+
 
 def oracle_sample(batch_x,batch_y,n,task):
     
     if task == "Classification":
-        a,b = oracle_Classification(batch_x,n)
+        a,b = oracle_Classification(batch_x,batch_y,n)
     elif task == "Recursion":
         a,b = oracle_Recursion(batch_x,batch_y,n)
     else:
         print("task-error!")
     return a,b
 
-def oracle_Classification(batch_x,n):
+def oracle_Classification(batch_x,batch_y,n):
     d = len(batch_x[1])
+    m=len(batch_x)
     
     a_w = [[0.0 for j in range(d)]for i in range(n)] 
     b_w = [[0.0 for j in range(1)]for i in range(n)] 
     a_w = np.array(a_w)
     b_w = np.array(b_w)
     for i in range(n):
-        
-        
-        
-        
+
         
         c= [[None for j in range(d)]for i in range(2)]
-        t = np.random.randint(d)
+        t = np.random.randint(m)
         s=t
         while(s==t):
-            s= np.random.randint(d)
+            s= np.random.randint(m)
+        
         
         c[0] = batch_x[t]
         c[1]= batch_x[s]
@@ -43,11 +85,60 @@ def oracle_Classification(batch_x,n):
         
         a_w[i] = (abs_a /np.linalg.norm(c[0]))*c[0] 
         b_w[i] = np.inner(a_w[i],c[0]) - z
+    
     return a_w,b_w
         
-        
+
+
+def oracle_Recursion(batch_x,batch_y,n):#一次元出力の回帰のオラクルサンプリング
+    sum_y = np.sum(batch_y,dtype="float")
+    d = len(batch_x[1])
+    nor = 1.0/sum_y
+    uni_y = nor*np.array(batch_y)
+    print(d)
     
+    
+    N = len(batch_y)
+    print(N)
+    a_w = [[0.0 for j in range(d)]for i in range(n)] 
+    b_w = [[0.0 for j in range(1)]for i in range(n)] 
+    a_w = np.array(a_w)
+    b_w = np.array(b_w)
+    for j in range(n):
+        u = np.random.uniform(0.0, 1.0)
+        now = 0
+        i = 0
         
+        t=0
+        while(now<u):
+            t +=1
+            print(t)
+            now += uni_y[i]
+            i += 1
+        t = i
+        s = -1
+        while(t !=s):
+            u = np.random.uniform(0.0, 1.0)
+            now = 0
+            i = 0
+            while(now<u):
+                now += uni_y[i]
+                i += 1
+            s = i
+        c= [[None for j in range(d)]for i in range(2)]
+        c[0] = batch_x[t]
+        c[1]= batch_x[s]
+        
+        zeta = np.random.beta(100,3)
+        gamma = np.random.binomial(1,0.5,1)
+        z = zeta*((-1)**gamma)
+        abs_a = 1.0/np.linalg.norm(c[1]-c[0])
+        
+        a_w[i] = (abs_a /np.linalg.norm(c[0]))*c[0] 
+        b_w[i] = np.inner(a_w[i],c[0]) - z
+    
+    return a_w,b_w
+
 
 
 
---
+++ ridglet2値.py
@@ -0,0 +1,107 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep  4 04:02:11 2018
+
+@author: 宮本来夏
+"""
+from tensorflow.examples.tutorials.mnist import input_data
+import tensorflow as tf
+import numpy as np
+import mine
+
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+
+
+n = 96
+m = 97
+
+
+
+x = tf.placeholder("float", [None, 784])
+W = tf.Variable(tf.random_normal([784,n], stddev=0.1))
+b = tf.Variable(tf.zeros([n]))
+y1 = tf.nn.relu(tf.add(tf.matmul(x,W) , b))
+W2 = tf.Variable(tf.random_normal([n,m],stddev=0.1))
+b2 = tf.Variable(tf.zeros([m]))
+y2 = tf.nn.relu(tf.matmul(y1,W2) + b2)
+W3 = tf.Variable(tf.random_normal([m,1],stddev=0.1))
+b3 = tf.Variable(tf.random_normal([1]))
+y = mine.double_class(tf.nn.sigmoid(tf.matmul(y2,W3) + b3))
+
+
+
+task = "Classification"
+
+
+batch_x, batch_y = mnist.train.next_batch(55000) #古バッチ
+
+batch_y_train_logi = mine.mnist_double(batch_y,"label")
+batch_y_test_logi = mine.mnist_double(mnist.test.labels,"label")
+
+batch_y_train = mine.mnist_double(batch_y,"OrS")
+
+
+
+task = "Classification"
+
+a1,b1 = mine.oracle_sample(batch_x,batch_y_train_logi,n,task)
+
+x_1 = np.matmul(a1,batch_x.T)+b1
+
+a2,b2_o = mine.oracle_sample(x_1.T ,batch_y_train_logi,m,task)
+
+
+
+
+W_o = tf.Variable(a1.T,dtype=tf.float32)
+b_o = tf.Variable(b1.T,dtype=tf.float32)
+
+y1_o = tf.nn.relu(tf.add(tf.matmul(x,W_o) , b_o))
+W2_o = tf.Variable(a2.T,dtype=tf.float32)
+b2_o = tf.Variable(b2_o.T,dtype=tf.float32)
+y2_o = tf.nn.relu(tf.add(tf.matmul(y1_o,W2_o) , b2_o))
+W3_o = tf.Variable(tf.random_normal([m,1],stddev=0.3))
+b3_o = tf.Variable(tf.random_normal([1]))
+
+y_o = mine.double_class(tf.nn.sigmoid(tf.add(tf.matmul(y2_o,W3_o) ,b3_o)),55000)
+
+
+
+
+
+y_ = tf.placeholder("float", [None,1])
+cross_entropy = -tf.reduce_sum(y_*tf.log(y))
+train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy,var_list=[W,b,W2,b2,W3,b3])
+
+cross_entropy_o = -tf.reduce_sum(y_*tf.log(y_o))
+train_step_o = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy_o,var_list=[W_o,b_o,W2_o,b2_o,W3_o,b3_o])
+correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+
+correct_prediction_o = tf.equal(tf.argmax(y_o,1), tf.argmax(y_,1))
+accuracy_o = tf.reduce_mean(tf.cast(correct_prediction_o, "float"))
+
+
+
+init = tf.initialize_all_variables()
+sess = tf.Session()
+sess.run(init)
+print (sess.run(accuracy, feed_dict={x: batch_x, y_: batch_y_train_logi}))
+print (sess.run(accuracy_o, feed_dict={x: batch_x, y_: batch_y_train_logi}))
+
+
+
+for i in range(1000):
+    
+    
+    batch_xs, batch_ys = mnist.train.next_batch(100)
+    batch_ys = mine.mnist_double(batch_ys,"label")
+    sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+    sess.run(train_step_o, feed_dict={x: batch_xs, y_: batch_ys})
+    
+    
+print (sess.run(accuracy, feed_dict={x: batch_x, y_: batch_y_train_logi}))
+print (sess.run(accuracy_o, feed_dict={x: batch_x, y_: batch_y_train_logi}))
+
+print (sess.run(accuracy, feed_dict={x: mnist.test.images, y_: batch_y_test_logi}))
+print (sess.run(accuracy_o, feed_dict={x: mnist.test.images, y_: batch_y_test_logi}))
